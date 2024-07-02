57b3d5b2d7c9901d3ad2c12da4af4c1cfeb8c1f0
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2024-07-02 13:20:35 -0400

    Add author and remove extra load message

--- NukeGates/Plugin.cs
+++ NukeGates/Plugin.cs
@@ -8,8 +8,9 @@ namespace NukeGates
 	public class Plugin : Plugin<Config>
 	{
 		private EventHandlers EventHandlers;
-		public override Version Version { get; } = new Version( 1, 3, 2 );
+		public override Version Version { get; } = new Version( 1, 3, 1 );
 		public override Version RequiredExiledVersion { get; } = new Version( 8, 0, 0 );
+		public override string Author { get; } = "OPGman";
 		public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
 		public override void OnEnabled()
@@ -18,7 +19,6 @@ namespace NukeGates
 			EventHandlers = new EventHandlers( this );
 			events.Warhead.Starting += EventHandlers.OnWarheadStart;
 			events.Player.InteractingDoor += EventHandlers.OnDoorUse;
-			Log.Info( $"Successfully loaded." );
 		}
 
 		public override void OnDisabled()
