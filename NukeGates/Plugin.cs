974d6fd56170edc882fd12719be3920d3184ff71
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-01-28 14:08:11 -0500

    Version bump

--- NukeGates/Plugin.cs
+++ NukeGates/Plugin.cs
@@ -8,8 +8,8 @@ namespace NukeGates
 	public class Plugin : Plugin<Config>
 	{
 		private EventHandlers EventHandlers;
-		public override Version Version { get; } = new Version( 1, 3, 3 );
-		public override Version RequiredExiledVersion { get; } = new Version( 8, 0, 0 );
+		public override Version Version { get; } = new Version( 1, 4, 0 );
+		public override Version RequiredExiledVersion { get; } = new Version( 9, 5, 0 );
 		public override string Author { get; } = "OPGman";
 		public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
