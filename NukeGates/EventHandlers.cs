c7c728da4e17a35ec692eed5e473307ad793f48b
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-01-28 12:47:16 -0500

    Fix all doors being locked when nuke is active

--- NukeGates/EventHandlers.cs
+++ NukeGates/EventHandlers.cs
@@ -26,7 +26,7 @@ namespace NukeGates
 
 		public void OnDoorUse( InteractingDoorEventArgs ev )
 		{
-			if ( Warhead.IsInProgress && plugin.Config.KeycardWhitelist.Length > 0 && ev.Player.CurrentItem != null )
+			if ( Warhead.IsInProgress && ev.Door.IsGate && plugin.Config.KeycardWhitelist.Length > 0 && ev.Player.CurrentItem != null )
 			{
 				if ( !plugin.Config.KeycardWhitelist.Contains( ev.Player.CurrentItem.Type ) )
 				{
